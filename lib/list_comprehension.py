1deffc48c553ca4b01025b3b5ca57ea872cf5bd0
laurenannmurugi <laurenann1871@gmail.com>
2025-08-08 00:53:55 +0300

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """Return a list of even integers from num_list."""
+    return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    """Return a list of sentences with exclamation marks at the end."""
+    return [sentence + "!" for sentence in sentence_list]
